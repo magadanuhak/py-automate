88f4f405a6b73c5fa88625d31bd3ac3875d82140
daniel <magadanuahak@gmail.com>
2022-08-09 01:05:07 +0300

    Format

--- Services/RunnerService.py
+++ Services/RunnerService.py
@@ -20,9 +20,11 @@ class RunnerService:
                         methodCallable = getattr(initializedTest, test_method)
                         methodCallable()
 
-                        print(colored(emoji.emojize(" :check_mark:") + ' [U] Done: ', 'green') + initializedTest.__module__ + "." + test_method )
+                        print(colored(emoji.emojize(" :check_mark:") + ' [U] Done: ',
+                                      'green') + initializedTest.__module__ + "." + test_method)
                     except Exception as e:
-                        print(colored(emoji.emojize(":cross_mark:") + " [U] Failed: ", 'red') + initializedTest.__module__ + "." + test_method + str(e))
+                        print(colored(emoji.emojize(":cross_mark:") + " [U] Failed: ",
+                                      'red') + initializedTest.__module__ + "." + test_method + str(e))
 
                         driver.quit()
 
@@ -32,8 +34,10 @@ class RunnerService:
                         methodCallable = getattr(initializedTest, auth_test_method)
                         methodCallable()
 
-                        print(colored(emoji.emojize(" :check_mark:") + " [A]  Done: ", 'green') + initializedTest.__module__ + "." + auth_test_method)
+                        print(colored(emoji.emojize(" :check_mark:") + " [A]  Done: ",
+                                      'green') + initializedTest.__module__ + "." + auth_test_method)
                     except Exception as e:
-                        print(colored(emoji.emojize(" :cross_mark:") + " [A] Failed: ", 'red') + initializedTest.__module__ + "." + auth_test_method + str(e))
+                        print(colored(emoji.emojize(" :cross_mark:") + " [A] Failed: ",
+                                      'red') + initializedTest.__module__ + "." + auth_test_method + str(e))
 
                         driver.quit()
