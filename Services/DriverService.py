643d7f205d534ec57a8c89fcd2e1130eec2ee001
daniel <magadanuahak@gmail.com>
2022-08-09 14:09:50 +0300

    Format

--- Services/DriverService.py
+++ Services/DriverService.py
@@ -1,12 +1,9 @@
-import selenium
-from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.opera import OperaDriverManager
 from selenium.webdriver.chrome.service import Service as BraveService
 from webdriver_manager.core.utils import ChromeType
 
